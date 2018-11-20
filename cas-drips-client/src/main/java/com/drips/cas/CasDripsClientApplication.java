894b0a3d3542bc22c88d2763272318afa30891c3
lijiabing <jackbing228@gmail.com>
2018-11-20 19:08:38 +0800

    Single Sign On Server and Client
    Single Sign On Server：cas-drips-server
    Single Sign On Client: cas-drips-client

---
+++ cas-drips-client/src/main/java/com/drips/cas/CasDripsClientApplication.java
@@ -0,0 +1,14 @@
+package com.drips.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018\11\20 0020.
+ */
+@SpringBootApplication
+public class CasDripsClientApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CasDripsClientApplication.class,args);
+    }
+}
---
+++ cas-drips-server/src/main/java/com/cas/server/CasDripsServerApplication.java
@@ -0,0 +1,14 @@
+package com.cas.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018\11\20 0020.
+ */
+@SpringBootApplication
+public class CasDripsServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CasDripsServerApplication.class,args);
+    }
+}
